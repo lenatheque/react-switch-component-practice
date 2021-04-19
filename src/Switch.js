90914cf981004a6723552abb98cf89b04f3b66fd
✬ ۞ ☾ Lorεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2021-04-19 11:13:35 -0600

    Initial commit

---
+++ src/Switch.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+var Switch = (props) => (
+  <div className="switch-plate">
+    <div className="screw" />
+    <div className="switch" onClick={props.toggle}>
+      <div className="switch-handle" />
+    </div>
+    <div className="screw" />
+  </div>
+);
+
+export default Switch;
